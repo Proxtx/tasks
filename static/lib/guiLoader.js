8c9af967261378dc4a6d973ee4a90ae7fafced3e
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-01-11 15:49:57 +0100

    some fighting with material ui... looks like i need to do some library fixes

--- static/lib/guiLoader.js
+++ static/lib/guiLoader.js
@@ -4,7 +4,6 @@ import { loadPack } from "/modules/uibuilder/main.js";
 
 await loadPack("/modules/material/components/pack.json", {
   urlPrefix: "/modules/material/",
-  customStyleSheets: ["../../lib/overwrite.css"],
 });
 
 await loadPack("/lib/components/pack.json", {
