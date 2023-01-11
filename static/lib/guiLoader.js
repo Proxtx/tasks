0e05c20803a8bc01b832739e837b867021e14b41
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-01-11 19:27:08 +0100

    importance and details sections

--- static/lib/guiLoader.js
+++ static/lib/guiLoader.js
@@ -4,6 +4,7 @@ import { loadPack } from "/modules/uibuilder/main.js";
 
 await loadPack("/modules/material/components/pack.json", {
   urlPrefix: "/modules/material/",
+  customStyleSheets: ["../../lib/overwrite.css"],
 });
 
 await loadPack("/lib/components/pack.json", {
